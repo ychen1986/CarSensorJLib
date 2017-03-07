b458583afab56635e6d0f9690e42b7360bffe4c9
yin <yin@dhcp199.ht.sfc.keio.ac.jp>
2017-03-07 17:27:19 +0900

    Add units to metadata

--- src/jp/ac/keio/sfc/ht/carsensor/sox/CreateSoxDevice.java
+++ src/jp/ac/keio/sfc/ht/carsensor/sox/CreateSoxDevice.java
@@ -3,8 +3,11 @@ package jp.ac.keio.sfc.ht.carsensor.sox;
 import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import jp.ac.keio.sfc.ht.sox.protocol.Device;
 import jp.ac.keio.sfc.ht.sox.protocol.DeviceType;
@@ -26,56 +29,96 @@ public class CreateSoxDevice {
 	static String soxPasswd = null;
 	static SoxConnection con = null;
 	static boolean debug = false;
+
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		parseOptions(args);
-		if (soxServer != null ){
+		if (soxServer != null) {
 			try {
 				debugMSG("Connect to sox server " + soxServer + "...");
 				con = new SoxConnection(soxServer, soxUser, soxPasswd, false);
+				// con = new SoxConnection(soxServer, false);
 			} catch (SmackException | IOException | XMPPException e) {
 				// TODO Auto-generated catch block
-				
-				System.err.println("Cannot connect to SoxServer" + soxServer );
+
+				System.err.println("Cannot connect to SoxServer " + soxServer);
 				e.printStackTrace();
 				System.exit(-1);
 			}
 			debugMSG("Done!");
 		}
 		
-		for(int i = 0; i <= 100; i++){
-			
-			String deviceName = "carsensor"+String.format("%03d", i)+"_100Hz";
+		for (int i = 0; i <= 100; i++) {
+
+			// String deviceName = "carsensor"+String.format("%03d",
+			// i)+"_100Hz";
+			String deviceName = "carsensor" + String.format("%03d", i);
 			createNewTypedDevice(deviceName);
 		}
 	}
-	static boolean createNewTypedDevice(String deviceName) {
-		String[] dataIds = { "Acceleration X", "Acceleration Y",
-				"Acceleration Z", "Angular Velocity X", "Angular Velocity Y",
-				"Angular Velocity Z", "Geomagnetism X", "Geomagnetism Y",
-				"Geomagnetism Z", "Atmospheric Pressure",
-				"Atmospheric Temperature", "Illuminance", "PM2.5",
-				"Satellite Number", "Longitude", "Latitude", "Altitude",
-				"Speed", "Cource", };
-		return createNewTypedDevice(deviceName, dataIds);
+
+	private static final Map<String, String> metaDatasMap;
+	static {
+		Map<String, String> metaDataMap = new HashMap();
+		metaDataMap.put("Data Index", "");
+		metaDataMap.put("Serial Number", "");
+		metaDataMap.put("Acceleration X", "m/s^2");
+		metaDataMap.put("Acceleration Y", "m/s^2");
+		metaDataMap.put("Acceleration Z", "m/s^2");
+		metaDataMap.put("Angular Velocity X", "rad/s");
+		metaDataMap.put("Angular Velocity Y", "rad/s");
+		metaDataMap.put("Angular Velocity Z", "rad/s");
+		metaDataMap.put("Geomagnetism X", "\u00B5T");
+		metaDataMap.put("Geomagnetism Y", "\u00B5T");
+		metaDataMap.put("Geomagnetism Z", "\u00B5T");
+		metaDataMap.put("Atmospheric Pressure", "hPa");
+		metaDataMap.put("Atmospheric Temperature", "°C");
+		metaDataMap.put("Atmospheric Humidity", "%RH");
+		metaDataMap.put("UV", "W/m^2");
+		metaDataMap.put("Illuminance", "Lx");
+		metaDataMap.put("PM2.5", "\u00B5g/m^3");
+		metaDataMap.put("Satellite Number", "");
+		metaDataMap.put("Longitude", "");
+		metaDataMap.put("Latitude", "");
+		metaDataMap.put("Altitude", "m");
+		metaDataMap.put("Speed", "km/hr");
+		metaDataMap.put("Cource", "°");
+
+		metaDatasMap = Collections.unmodifiableMap(metaDataMap);
 	}
+	/*
+	 * static boolean createNewTypedDevice(String deviceName) { String[] dataIds
+	 * = { "Data Index", "Serial Number", "Acceleration X", "Acceleration Y",
+	 * "Acceleration Z", "Angular Velocity X", "Angular Velocity Y",
+	 * "Angular Velocity Z", "Geomagnetism X", "Geomagnetism Y",
+	 * "Geomagnetism Z", "Atmospheric Pressure", "Atmospheric Temperature",
+	 * "Atmospheric Humidity", "UV", "Illuminance", "PM2.5", "Satellite Number",
+	 * "Longitude", "Latitude", "Altitude", "Speed", "Cource"}; return
+	 * createNewTypedDevice(deviceName, dataIds); }
+	 */
 
-	private static boolean createNewTypedDevice(String deviceName,
-			String[] dataIds) {
-
+	// private static boolean createNewTypedDevice(String deviceName,
+	// String[] dataIds) {
+	static boolean createNewTypedDevice(String deviceName) {
 		Device device = new Device();
 		device.setId(deviceName);
 		device.setDeviceType(DeviceType.OUTDOOR_WEATHER);
 		device.setName(deviceName);
 
 		List<Transducer> transducers = new ArrayList<Transducer>();
-
-		for (String s : dataIds) {
+		for (Map.Entry<String, String> entry : metaDatasMap.entrySet()) {
 			Transducer metaValue = new Transducer();
-			metaValue.setName(s);
-			metaValue.setId(s);
+			metaValue.setName(entry.getKey());
+			metaValue.setId(entry.getKey());
+			metaValue.setUnits(entry.getValue());
 			transducers.add(metaValue);
+
 		}
+		/*
+		 * for (String s : dataIds) { Transducer metaValue = new Transducer();
+		 * metaValue.setName(s); metaValue.setId(s); metaValue.setUnits(arg0);
+		 * transducers.add(metaValue); }
+		 */
 
 		device.setTransducers(transducers);
 
@@ -86,27 +129,24 @@ public class CreateSoxDevice {
 				debugMSG("Create new typed device " + deviceName + "...");
 			}
 			try {
-			
-				con.createNode(deviceName, device, AccessModel.open,
-						PublishModel.open);
+
+				con.createNode(deviceName, device, AccessModel.open, PublishModel.open);
 				debugMSG("done!");
 
 				return true;
-			} catch (NoResponseException | XMPPErrorException
-					| NotConnectedException e) {
+			} catch (NoResponseException | XMPPErrorException | NotConnectedException e) {
 				// TODO Auto-generated catch block
 				debugMSG("fail!");
 				e.printStackTrace();
-//				try {
-//					//Thread.sleep(1000);
-//					con.deleteNode("deviceName");
-//				} catch (NoResponseException | XMPPErrorException
-//						| NotConnectedException | InterruptedException e1) {
-//					// TODO Auto-generated catch block
-//					e1.printStackTrace();
-//				}
-				
-				
+				// try {
+				// //Thread.sleep(1000);
+				// con.deleteNode("deviceName");
+				// } catch (NoResponseException | XMPPErrorException
+				// | NotConnectedException | InterruptedException e1) {
+				// // TODO Auto-generated catch block
+				// e1.printStackTrace();
+				// }
+
 			}
 			try {
 				Thread.sleep(1000);
@@ -118,38 +158,35 @@ public class CreateSoxDevice {
 		return false;
 
 	}
+
 	static void infoMSG(String msg) {
 		{
 			Date now = new Date();
-			SimpleDateFormat sdfDate = new SimpleDateFormat(
-					"yyyy-MM-dd HH:mm:ss.SSSS");// dd/MM/yyyy
+			SimpleDateFormat sdfDate = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSSS");// dd/MM/yyyy
 
 			String strDate = sdfDate.format(now);
 
-			System.out.println("[" + sdfDate.format(now) + "] " + "["
-				 + "] " + msg);
+			System.out.println("[" + sdfDate.format(now) + "] " + "[" + "] " + msg);
 		}
 	}
 
 	static void debugMSG(String msg) {
 		if (debug) {
 			Date now = new Date();
-			SimpleDateFormat sdfDate = new SimpleDateFormat(
-					"yyyy-MM-dd HH:mm:ss.SSSS");// dd/MM/yyyy
+			SimpleDateFormat sdfDate = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSSS");// dd/MM/yyyy
 
 			String strDate = sdfDate.format(now);
 
-			System.out.println("[" + sdfDate.format(now) + "] " + "["
-					+ "] " + msg);
+			System.out.println("[" + sdfDate.format(now) + "] " + "[" + "] " + msg);
 		}
 	}
+
 	protected static void parseOptions(String[] args) {
-		
-		String usage =  "Usage: java -jar CreateSoxDevice.jar -s <soxServer> -u <soxUser> -p <soxPasswd> -debug <true/flase>";
+
+		String usage = "Usage: java -jar CreateSoxDevice.jar -s <soxServer> -u <soxUser> -p <soxPasswd> -debug <true/flase>";
 		if (args.length == 0) {
 			System.err.println("ERROR: arguments required!");
-			System.err
-					.println(usage);
+			System.err.println(usage);
 			System.exit(1);
 		}
 		for (int i = 0; i < args.length; i++) {
@@ -159,15 +196,14 @@ public class CreateSoxDevice {
 				soxUser = args[++i];
 			} else if (args[i].equals("-p")) {
 				soxPasswd = args[++i];
-			} else if(args[i].equals("-debug")){
-				String debugPara= args[++i];
-				if (debugPara.equals("true")){
+			} else if (args[i].equals("-debug")) {
+				String debugPara = args[++i];
+				if (debugPara.equals("true")) {
 					debug = true;
 				}
-			}else {
+			} else {
 				System.err.println("ERROR: invalid option " + args[i]);
-				System.err
-						.println(usage);
+				System.err.println(usage);
 				System.exit(1);
 			}
 		}
